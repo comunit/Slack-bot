cf520e264d89633d2b8ad3a72b6fa93e6425b61f
Imran Rafique <comunit76@gmail.com>
2018-06-05 20:25:27 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,94 @@
+const SlackBot = require('slackbots');
+const axios = require('axios');
+
+const bot = new SlackBot({
+  token: 'xoxb-375608689809-375458852848-Y1hVqf03ch4tkLrigIJkwxdR',
+  name: 'jokebot'
+});
+
+// start handler
+bot.on('start', () => {
+  const params = {
+    icon_emoji: ':smily'
+  };
+
+  bot.postMessageToChannel('general', 'Get ready to laugh with @jokebot',
+    params
+  );
+});
+
+// error handle
+bot.on('error', (err) => console.log(err));
+
+// message handler
+bot.on('message', (data) => {
+  if (data.type !== 'message') {
+    return;
+  }
+
+  handleMessage(data.text);
+});
+
+// respond to data
+function handleMessage(message) {
+  if (message.includes(' chucknorris')) {
+    chuckJoke();
+  } else if (message.includes(' yomama')) {
+    youMomaJoke();
+  } else if (message.includes(' random')) {
+    randomJoke();
+  } else if (message.includes(' help')) {
+    runhelp();
+  }
+}
+
+// tell chuck norris joke
+function chuckJoke() {
+  axios.get('http://api.icndb.com/jokes/random')
+    .then(res => {
+      const joke = res.data.value.joke;
+      const params = {
+        icon_emoji: ':laughing'
+      };
+
+      bot.postMessageToChannel('general',
+        `Chuck Norris: ${joke}`,
+        params
+      );
+    })
+}
+
+// tell a you mama joke
+function youMomaJoke() {
+  axios.get('http://api.yomomma.info')
+    .then(res => {
+      const joke = res.data.joke;
+      const params = {
+        icon_emoji: ':laughing'
+      };
+
+      bot.postMessageToChannel('general',
+        `Yo Mama: ${joke}`,
+        params
+      );
+    })
+}
+
+// tell a random joke
+function randomJoke() {
+  const rand = Math.floor(Math.random() * 2) + 1;
+  if (rand === 1) {
+    chuckJoke();
+  } else if (rand === 2) {
+    youMomaJoke();
+  }
+}
+
+// run help
+function runhelp() {
+  const params = {
+    icon_emoji: ':question'
+  };
+
+  bot.postMessageToChannel('general',`Type @ jokebot with either 'chucknorris', 'yomoma' or 'random' to get a joke`,params);
+}
